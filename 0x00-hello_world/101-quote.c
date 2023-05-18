068c531a0d9f005e9f67e93d8b78d4d6f51f8627
Abdessamad El Majdouly <elmajdouly.abdessamad@gmail.com>
2023-05-18 15:26:28 -0700

    first commit1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,32 @@
-#include <stdio.h>
-#include <unistd.h>
+/*
+******************************************************************************
+*                                                                            *
+*                                                 _____  ______    ____  ___ *
+*    101-quote.c                                 /  _  \ |    |    \   \/  / *
+*                                               /  /_\  \|    |     \     /  *
+*    By: Barahmou <hamabarhamou@gmail.com>     /    |    \    |___  /     \  *
+*                                              \____|__  /_______ \/___/\  \ *
+*    Created: 2022-03-10 13:29:43 by Barahmou          \/        \/      \_/ *
+*    Updated: 2022-03-10 13:29:43 by Barahmou                                *
+*                                                                            *
+******************************************************************************
+*/
+
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+
 /**
- * main - Entry point
- * Return:1
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	system("rm barhamounafissa > sorti 2>&1");
+	exit(1);
 }
+
