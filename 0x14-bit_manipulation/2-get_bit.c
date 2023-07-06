24fbb70d25d1f8a1203b608579bc1da97fe39c37
Abdessamad El Majdouly <elmajdouly.abdessamad@gmail.com>
2023-07-06 14:34:32 -0700

    2-get_bit.c v4

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,18 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int i;
 
-	if (index >= sizeof(n) * 8)
-		return (-1);
+	if (n == 0 && index < 64)
+		return (0);
 
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
 
-	return (n >> index & 1);
+	return (-1);
 }
-
-
