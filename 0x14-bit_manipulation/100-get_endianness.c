877fb42c5909e2b4719c146ddbbf60bfce2c61ce
Abdessamad El Majdouly <elmajdouly.abdessamad@gmail.com>
2023-07-06 14:46:56 -0700

    100-get_endianness.c v2

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -9,9 +9,17 @@
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *) &i;
+int n;
 
-	return (*c);
+    n = get_endianness();
+    if (n != 0)
+    {
+        printf("Little Endian\n");
+    }
+    else
+    {
+        printf("Big Endian\n");
+    }
+    return 0;
 }
 
