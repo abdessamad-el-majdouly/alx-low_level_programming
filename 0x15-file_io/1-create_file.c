b0f17211a4bf14f000adad2f1e0e0f3818b22d93
abdessamad-el-majdouly <elmajdouly.abdessamad@gmail.com>
2023-08-07 16:04:51 -0700

    0x15-file_io 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,49 +1,46 @@
 /*****************************************************************************************************/
 /*                                  						                                         */
 /*                                  						             _____  ______    ____  ___  */
-/* 0-print_list.c                   						            /  _  \ |    |    \   \/  /  */
+/* 1-create_file.c                   						            /  _  \ |    |    \   \/  /  */
 /*                                  						           /  /_\  \|    |     \     /   */
 /* By: Abdessamad El Majdouly   <elmajdouly.abdessamad@gmail.com>  	  /    |    \    |___  /     \   */
 /*                                   						      	  \____|__  /_______ \/___/\  \  */
 /* Created: 2023-08-07 09:44:03   $Abdessamad   					   	      \/        \/      \_/  */
-/* Updated: 2023-08-07 09:44:03 by Abdessamad                             							 */
+/* Updated: 2023-08-07 09:44:03 by Abdessamad                         							 */
 /*                                                                         						     */
 /*****************************************************************************************************/
-
-#include <stdio.h>
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-
-#define BUF_SIZE 9096
 
 /**
- * create_file - a function ...
- * @filename: the list
- * @text_content: the number
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 or 0
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, 600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	chmod(filename, 644);
-
-	if (text_content != NULL)
-		write(fd, text_content, strlen(text_content));
 	close(fd);
+
 	return (1);
 }
+
 
