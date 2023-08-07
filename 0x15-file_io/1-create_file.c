50b3dfe08a33a7e3acf8cc1fc4d19cf2732c291a
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2023-08-08 00:12:42 +0100

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,46 +1,37 @@
-/*****************************************************************************************************/
-/*                                  						                                         */
-/*                                  						             _____  ______    ____  ___  */
-/* 1-create_file.c                   						            /  _  \ |    |    \   \/  /  */
-/*                                  						           /  /_\  \|    |     \     /   */
-/* By: Abdessamad El Majdouly   <elmajdouly.abdessamad@gmail.com>  	  /    |    \    |___  /     \   */
-/*                                   						      	  \____|__  /_______ \/___/\  \  */
-/* Created: 2023-08-07 09:44:03   $Abdessamad   					   	      \/        \/      \_/  */
-/* Updated: 2023-08-07 09:44:03 by Abdessamad                         							 */
-/*                                                                         						     */
-/*****************************************************************************************************/
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "abdessamad.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		i++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1 || w == -1)
+	if (file == -1)
 		return (-1);
 
-	close(fd);
+	write(file, text_content, i);
 
 	return (1);
 }
-
-
